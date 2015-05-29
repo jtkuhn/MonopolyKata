25675fe5c3a2a72356d5679e8ef40c7ea13f451c
Jordan Kuhn <jtkuhn@foxguardsolutions.com>
2015-05-29 15:31:46 -0400

    Added unit tests for banker because that's probably a good idea

--- UnitTestProject1/BankerTest.cs
+++ UnitTestProject1/BankerTest.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MonopolyKata;
+using NUnit.Framework;
 
 namespace UnitTestProject1
 {
-    class BankerTest
+    [TestFixture]
+    public class BankerTest
     {
+        private Player player1;
+        private Banker banker;
+
+        [SetUp]
+        public void Setup()
+        {
+            banker = new Banker();
+            player1 = new Player("Bob");
+        }
+
+        [Test]
+        public void WhenBankerTakesMoneyFromPlayer_HisMoneyDecrementsAccordingly()
+        {
+            Assert.AreEqual(1500, player1.Money);
+            banker.TakeMoneyFromPlayer(player1, 150);
+            Assert.AreEqual(1350, player1.Money);
+        }
+
+        [Test]
+        public void WhenBankerGivesMoneyToPlayer_HisMoneyIncrementsAccordingly()
+        {
+            Assert.AreEqual(1500, player1.Money);
+            banker.GiveMoneyToPlayer(player1, 150);
+            Assert.AreEqual(1650, player1.Money);
+        }
+
+        [Test]
+        public void WhenBankerTransfersMoneyBetweenTwoPlayers_ItWorksAsItShould()
+        {
+            Player player2 = new Player("Bill");
+            Assert.AreEqual(1500, player1.Money);
+            Assert.AreEqual(1500, player2.Money);
+
+            banker.TransferMoney(player1, player2, 100);
+            Assert.AreEqual(1600, player1.Money);
+            Assert.AreEqual(1400, player2.Money);
+        }
     }
 }
